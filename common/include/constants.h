8c9a685f8b91958627fe0bceec8c9fa7a8629fe6
lipton <liptonmartin135@gmail.com>
2026-04-26 14:53:51 +0300

    insert table

---
+++ common/include/constants.h
@@ -0,0 +1,14 @@
+﻿//
+// Created by rvova on 26.04.2026.
+//
+
+#ifndef MINIDB_CONSTANTS_H
+#define MINIDB_CONSTANTS_H
+
+#include <cstddef>
+
+namespace db {
+    inline constexpr size_t PAGE_SIZE = 4096;
+}
+
+#endif //MINIDB_CONSTANTS_H
---
+++ common/tests/test_common.cpp
@@ -0,0 +1,22 @@
+﻿//
+// Created by rvova on 26.04.2026.
+//
+
+#include <iostream>
+
+#include "datatypes.h"
+
+int main() {
+    Database database("vova");
+
+    std::vector<Column> columns;
+    columns.emplace_back("column_1", DataType::Int);
+    database.insert_table("table_2", columns);
+
+    auto tables = database.get_tables();
+    for (const auto& item : tables) {
+        std::cout << item << ' ';
+    }
+
+    return 0;
+}
