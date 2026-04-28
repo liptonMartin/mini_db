a73054105f4936a2ac6df84db5ddaf1a8da7b8b4
dader233 <danniy.parol@gmail.com>
2026-04-28 22:39:47 +0300

    keywords to start lexer

--- server/include/lexer.h
+++ server/include/lexer.h
@@ -39,5 +39,35 @@ struct Token {
     std::string value;
 };
 
+// Ключевые слова с правилами
+struct kw_create   : TAO_PEGTL_STRING("CREATE") {};
+struct kw_database : TAO_PEGTL_STRING("DATABASE") {};
+struct kw_drop     : TAO_PEGTL_STRING("DROP") {};
+struct kw_use      : TAO_PEGTL_STRING("USE") {};
+struct kw_table    : TAO_PEGTL_STRING("TABLE") {};
+struct kw_not      : TAO_PEGTL_STRING("NOT") {};
+struct kw_null     : TAO_PEGTL_STRING("NULL") {};
+struct kw_indexed  : TAO_PEGTL_STRING("INDEXED") {};
+struct kw_insert   : TAO_PEGTL_STRING("INSERT") {};
+struct kw_into     : TAO_PEGTL_STRING("INTO") {};
+struct kw_value   : TAO_PEGTL_STRING("VALUE") {};
+struct kw_update   : TAO_PEGTL_STRING("UPDATE") {};
+struct kw_set      : TAO_PEGTL_STRING("SET") {};
+struct kw_delete   : TAO_PEGTL_STRING("DELETE") {};
+struct kw_from     : TAO_PEGTL_STRING("FROM") {};
+struct kw_select   : TAO_PEGTL_STRING("SELECT") {};
+struct kw_where    : TAO_PEGTL_STRING("WHERE") {};
+struct kw_as       : TAO_PEGTL_STRING("AS") {};
+struct kw_and      : TAO_PEGTL_STRING("AND") {};
+struct kw_like     : TAO_PEGTL_STRING("LIKE") {};
+struct kw_between  : TAO_PEGTL_STRING("BETWEEN") {};
+
+
+struct keyword : pegtl::sor<
+    kw_create, kw_database, kw_drop, kw_use, kw_table,
+    kw_not, kw_null, kw_indexed, kw_insert, kw_into,
+    kw_value, kw_update, kw_set, kw_delete, kw_from, kw_select,
+    kw_where, kw_as, kw_and, kw_like, kw_between
+>{};
 
 #endif //MINI_DB_PARSER_H
