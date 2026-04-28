1a0537e92e6c6060054bf2fc0f780bd0c6c8520b
dader233 <danniy.parol@gmail.com>
2026-04-28 23:58:15 +0300

    Tokenizification done

--- server/src/lexer.cpp
+++ server/src/lexer.cpp
@@ -1,4 +1,33 @@
+#include "lexer.h"
+#include <tao/pegtl.hpp>
 
-#include "../include/lexer.h"
-#include <iostream>
-#include "sq"
+namespace pegtl = tao::pegtl;
+
+
+template<typename Rule>
+struct action : pegtl::nothing<Rule> {};  // ЗАГЛУШКА - ДЕЛАТЬ НИЧЕГО ЕСЛИ ЧИТАЕМ
+
+template<> struct action<keyword> { // ACTION для KEYWORD, видим KEYWORD тогда вызываем apply и добаляем объект токен в конец вкетора
+    template<typename Input>
+    static void apply(const Input& in, std::vector<Token>& v) {
+        v.push_back({TypeToken::KEYWORD, in.string()});
+    }
+};
+
+// TODO: НАКЛЕПАТЬ ТАКИХ ЖЕ ЭКШНОВ ДЛЯ КАЖДОГО ТОКЕНТАЙПА - обработать их ошибки, В ТОКЕНАЙЗ И ПРИДУМАТЬ ЧТО ДЕЛАТЬ ДАЛЬШЕ
+struct spaces : pegtl::star<pegtl::space> {};
+struct grammar : pegtl::list<token, spaces> {};
+
+std::vector<Token> Lexer::tokenize(const std::string& input) {
+    std::vector<Token> tokens;
+    try {
+        pegtl::memory_input<> in(input, "lexer_sql");
+        pegtl::parse<grammar, action>(in, tokens);
+    } catch (const pegtl::parse_error& e) {
+        throw LexerException(
+            "Unexpected character at position " +
+            std::to_string(e.positions.front().byte)
+        );
+    }
+    return tokens;
+}
