756be5825c73254275d259e2b80d0e99500ce4c8
Artem Mitskevich <echidnartem@gmail.com>
2026-04-29 21:58:09 +0300

    add page manager implementation

--- common/include/datatypes.h
+++ common/include/datatypes.h
@@ -3,11 +3,16 @@
 
 #ifndef MINIDB_DATATYPES_H
 #define MINIDB_DATATYPES_H
+#include <any>
+#include <cstddef>
 #include <filesystem>
 #include <vector>
 #include <string>
 #include <fstream>
+#include <optional>
+#include <variant>
 
+using Values = std::variant<int, std::string>;
 
 /**
  * _free_size - количество в байтах свободного места на странице
@@ -119,7 +124,7 @@ public:
 
     std::vector<char> get_page_data();
 
-    void insert_element(const std::vector<char> &data);
+    ptrdiff_t insert_element(const std::vector<char> &data);
 
     void erase_element(ptrdiff_t slot_id);
 
@@ -129,17 +134,87 @@ public:
     std::vector<std::vector<char> > get_slots_data();
 };
 
+class PageManager {
+    std::fstream &_file;
+    ptrdiff_t _pages_begin_offset;
+
+    ptrdiff_t page_offset(ptrdiff_t page_id) const;
+
+public:
+    PageManager(std::fstream &file, ptrdiff_t pages_begin_offset);
+
+    ptrdiff_t allocate_page();
+
+    ptrdiff_t search_free_page(ptrdiff_t need_size);
+
+    std::vector<char> read_page(ptrdiff_t page_id);
+
+    void write_page(ptrdiff_t page_id, const std::vector<char> &data);
+};
+
 enum class DataType { Int, String };
 
 class Column {
-    std::string _name;
+    std::string _name = "";
     DataType _type;
 
     friend class Table;
 
 public:
+    Column() = default;
+
     explicit Column(const std::string &name, const DataType type);
 };
+
+
+enum class ComparisonDataType {
+    Equal,
+    NotEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+};
+
+
+class Condition {
+    Column _column{};
+
+public:
+    virtual ~Condition() = default;
+
+    virtual bool evaluate() const = 0;
+};
+
+class ComparisonCondition : public Condition {
+    ComparisonDataType _comparison_type;
+    std::any _value;
+
+public:
+    ComparisonCondition(Column column, ComparisonDataType comparison_type, std::any value);
+
+    bool evaluate() const override;
+};
+
+class BetweenCondition : public Condition {
+    Column _column_start;
+    Column _column_end;
+
+public:
+    BetweenCondition(Column column, Column column_start, Column column_end);
+
+    bool evaluate() const override;
+};
+
+class RegexCondition : public Condition {
+    std::string _regex;
+
+public:
+    RegexCondition(Column column, std::string regex);
+
+    bool evaluate() const override;
+};
+
 
 /**
  * На первой страницы файла {name}.binary хранится :
@@ -163,6 +238,14 @@ public:
     std::vector<Column> get_columns();
 
     std::vector<Page> get_pages();
+
+    void insert_elements(); // TODO: edit declaration
+
+    void update_elements(); // TODO: edit declaration
+
+    void delete_elements(); // TODO: edit declaration
+
+    std::vector<Values> select_elements(); // TODO: edit declaration
 };
 
 /**
@@ -175,22 +258,42 @@ public:
 class Database {
     std::fstream _file;
 
-    static std::filesystem::path make_path_to_file(const std::string &name);
+    static std::filesystem::path make_path_to_file(const std::string &db_name);
 
     void move_to_position_tables_begin();
 
     void move_to_position_tables_end();
 
+    explicit Database(const std::string &name, bool need_to_create);
+
 public:
-    explicit Database(const std::string &name);
+    static Database create_database(const std::string &name);
+    static Database load_database(const std::string &name);
+
+    /**
+     *
+     * @param db_name Имя базы данных
+     * @exception DatabaseDoesNotExistException Попытка удалить несуществующей базы данных
+     */
+    void drop_database(const std::string& db_name);
 
     std::string get_name();
 
     std::vector<std::string> get_tables();
 
-    void insert_table(const std::string &name, const std::vector<Column> &columns);
+    void create_table(const std::string &name, const std::vector<Column> &columns);
 
-    void drop_table(const std::string &name);
+    void drop_table(const std::string &name); // TODO: impl it!
+
+    void insert_elements(const std::string &table_name, const std::vector<Column> &columns,
+                         const std::vector<std::variant<int, std::string> > &values); // TODO: impl it!
+
+    void update_elements(const std::string &table_name, const Condition &condition, const std::vector<Column> &columns,
+                         const std::vector<Values> &values); // TODO: impl it!
+
+    void delete_elements(const std::string &table_name, const Condition &condition); // TODO: impl it!
+
+    std::vector<Values> select_elements(const std::string &table_name, const std::optional<Condition> &condition);
 };
 
 
---
+++ common/src/page_manager.cpp
@@ -0,0 +1,118 @@
+#include "datatypes.h"
+
+#include <format>
+#include <stdexcept>
+
+#include "constants.h"
+
+PageManager::PageManager(std::fstream &file, const ptrdiff_t pages_begin_offset)
+    : _file(file), _pages_begin_offset(pages_begin_offset) {
+    if (_pages_begin_offset < 0) {
+        throw std::invalid_argument("Pages begin offset cannot be negative");
+    }
+}
+
+ptrdiff_t PageManager::page_offset(const ptrdiff_t page_id) const {
+    if (page_id < 0) {
+        throw std::invalid_argument("Page id cannot be negative");
+    }
+
+    return _pages_begin_offset + page_id * static_cast<ptrdiff_t>(db::PAGE_SIZE);
+}
+
+ptrdiff_t PageManager::allocate_page() {
+    if (!_file.is_open()) {
+        throw std::runtime_error("PageManager file is not open");
+    }
+
+    _file.clear();
+    _file.seekp(0, std::ios::end);
+    const auto end_position = _file.tellp();
+    if (end_position == std::streampos(-1)) {
+        throw std::runtime_error("Cannot determine file size");
+    }
+
+    const auto end_offset = static_cast<ptrdiff_t>(end_position);
+    if (end_offset < _pages_begin_offset) {
+        throw std::runtime_error("File ends before pages begin offset");
+    }
+
+    const auto pages_bytes = end_offset - _pages_begin_offset;
+    if (pages_bytes % static_cast<ptrdiff_t>(db::PAGE_SIZE) != 0) {
+        throw std::runtime_error("Page area size is not aligned to PAGE_SIZE");
+    }
+
+    const auto page_id = pages_bytes / static_cast<ptrdiff_t>(db::PAGE_SIZE);
+    auto page = Page::create_page();
+    write_page(page_id, page.get_page_data());
+    return page_id;
+}
+
+ptrdiff_t PageManager::search_free_page(const ptrdiff_t need_size) {
+    if (need_size < 0) {
+        throw std::invalid_argument("Need size cannot be negative");
+    }
+
+    _file.clear();
+    _file.seekg(0, std::ios::end);
+    const auto end_position = _file.tellg();
+    if (end_position == std::streampos(-1)) {
+        throw std::runtime_error("Cannot determine file size");
+    }
+
+    const auto end_offset = static_cast<ptrdiff_t>(end_position);
+    if (end_offset < _pages_begin_offset) {
+        return -1;
+    }
+
+    const auto pages_bytes = end_offset - _pages_begin_offset;
+    if (pages_bytes % static_cast<ptrdiff_t>(db::PAGE_SIZE) != 0) {
+        throw std::runtime_error("Page area size is not aligned to PAGE_SIZE");
+    }
+
+    const auto pages_count = pages_bytes / static_cast<ptrdiff_t>(db::PAGE_SIZE);
+    for (ptrdiff_t page_id = 0; page_id < pages_count; ++page_id) {
+        Page page(read_page(page_id));
+        if (page.get_free_size() >= need_size) {
+            return page_id;
+        }
+    }
+
+    return -1;
+}
+
+std::vector<char> PageManager::read_page(const ptrdiff_t page_id) {
+    if (!_file.is_open()) {
+        throw std::runtime_error("PageManager file is not open");
+    }
+
+    std::vector<char> data(db::PAGE_SIZE);
+
+    _file.clear();
+    _file.seekg(page_offset(page_id), std::ios::beg);
+    _file.read(data.data(), static_cast<std::streamsize>(data.size()));
+
+    if (_file.gcount() != static_cast<std::streamsize>(db::PAGE_SIZE)) {
+        throw std::runtime_error(std::format("Cannot read page {}", page_id));
+    }
+
+    return data;
+}
+
+void PageManager::write_page(const ptrdiff_t page_id, const std::vector<char> &data) {
+    if (!_file.is_open()) {
+        throw std::runtime_error("PageManager file is not open");
+    }
+
+    if (data.size() != db::PAGE_SIZE) {
+        throw std::invalid_argument("Page data size must be equal to PAGE_SIZE");
+    }
+
+    _file.clear();
+    _file.seekp(page_offset(page_id), std::ios::beg);
+    _file.write(data.data(), static_cast<std::streamsize>(data.size()));
+
+    if (!_file) {
+        throw std::runtime_error(std::format("Cannot write page {}", page_id));
+    }
+}
