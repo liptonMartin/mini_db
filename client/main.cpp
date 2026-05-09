7c205335e26fb6cd62344c48b9ef627e42f94a5f
Vladimir Rybin <liptonmartin135@gmail.com>
2026-05-09 13:03:14 +0300

    client-server (#27)

---
+++ client/main.cpp
@@ -0,0 +1,165 @@
+﻿//
+// Created by rvova on 08.05.2026.
+//
+
+#include <winsock2.h>
+#include <iostream>
+#include <ws2tcpip.h>
+#include <nlohmann/json.hpp>
+
+#include "exceptions.h"
+
+
+class ClientSocket {
+    SOCKET _server_socket = INVALID_SOCKET;
+
+    void shutdown() {
+        if (_server_socket != INVALID_SOCKET) {
+            closesocket(_server_socket);
+            _server_socket = INVALID_SOCKET;
+        }
+
+        WSACleanup();
+        std::cout << "The client socket was shutdown!";
+    }
+
+public:
+    explicit ClientSocket(const int port) {
+        WSADATA wsa_data;
+        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+            std::cout << "WSAStartup failed\n";
+            throw FailedStartSocketException();
+        }
+
+        _server_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (_server_socket == INVALID_SOCKET) {
+            std::cout << "Could not create socket\n";
+            WSACleanup();
+            throw FailedStartSocketException();
+        }
+
+        sockaddr_in client_address{};
+        client_address.sin_family = AF_INET;
+        InetPton(AF_INET, "127.0.0.1", &client_address.sin_addr);
+        client_address.sin_port = htons(port);
+
+        if (connect(_server_socket, reinterpret_cast<sockaddr *>(&client_address), sizeof(client_address))) {
+            std::cout << "Could not connect to server\n";
+            shutdown();
+            throw FailedStartSocketException();
+        }
+
+        std::cout << "Connected to server\n";
+    }
+
+    void send_data_to_server(std::string &&data) {
+        if (_server_socket == INVALID_SOCKET) {
+            std::cout << "Could not connect to server\n";
+            shutdown();
+            throw FailedSendDataException();
+        }
+        /* сначала отправляем длину сообщения */
+        const uint32_t length_data = data.length();
+        const auto res = send(_server_socket, reinterpret_cast<const char *>(&length_data), sizeof(length_data), 0);
+        if (res == SOCKET_ERROR) {
+            std::cout << "Failed to send data to server\n";
+            shutdown();
+            throw FailedSendDataException();
+        }
+
+        /* затем отправляем сами данные */
+        if (send(_server_socket, data.c_str(), static_cast<int>(data.length()), 0) == SOCKET_ERROR) {
+            std::cout << "Failed to send data to server\n";
+            shutdown();
+            throw FailedSendDataException();
+        }
+        std::cout << "Data was sent to server successfully\n";
+    }
+
+    std::string receive_data_from_server() {
+        if (_server_socket == INVALID_SOCKET) {
+            std::cout << "Could not connect to server\n";
+            shutdown();
+            throw FailedReceiveDataException();
+        }
+
+        /* сначала считываем длину сообщения */
+        uint32_t length_data;
+        int count_bytes = recv(_server_socket, reinterpret_cast<char *>(&length_data), sizeof(length_data), 0);
+        if (count_bytes == SOCKET_ERROR) {
+            std::cout << "Failed to receive data from server\n";
+            shutdown();
+            throw FailedReceiveDataException();
+        }
+        if (count_bytes == 0) {
+            std::cout << "Server has internal error!\n";
+            shutdown();
+            throw InternalServerErrorException();
+        }
+
+        std::string data;
+        data.resize(length_data);
+        count_bytes = recv(_server_socket, data.data(), static_cast<int>(data.length()), 0);
+        if (count_bytes == SOCKET_ERROR) {
+            std::cout << "Failed to receive data from server\n";
+            shutdown();
+            throw FailedReceiveDataException();
+        }
+        if (count_bytes == 0) {
+            std::cout << "Server has internal error!\n";
+            shutdown();
+            throw InternalServerErrorException();
+        }
+        return data;
+    }
+
+    ~ClientSocket() {
+        shutdown();
+    }
+};
+
+
+void help() {
+    std::cout << "Usage: ./client PORT\n";
+}
+
+void print_message_input_command() {
+    std::cout << "Please input your command (Ctrl+C for exit):\n";
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        help();
+        return -1;
+    }
+
+    int port;
+    try {
+        port = std::stoi(argv[1]);
+    } catch (std::exception &) {
+        std::cout << "Invalid format for PORT\n";
+        help();
+        return -1;
+    }
+
+    ClientSocket client_socket(port);
+
+    print_message_input_command();
+    std::string data;
+    while (std::getline(std::cin, data)) {
+        client_socket.send_data_to_server(std::move(data));
+        std::string response;
+        try {
+            response = client_socket.receive_data_from_server();
+        } catch (InternalServerErrorException &e) {
+            std::cout << e.what() << "\n";
+            return 1;
+        }
+
+        auto json_response = nlohmann::json::parse(response);
+        std::cout << json_response << "\n";
+        print_message_input_command();
+    }
+
+    return 0;
+}
--- server/main.cpp
+++ server/main.cpp
@@ -2,4 +2,172 @@
 // Created by rvova on 28.04.2026.
 //
 
-// точка входа на сервер
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "exceptions.h"
+
+class ServerSocket {
+    SOCKET _listen_socket = INVALID_SOCKET;
+    SOCKET _client_socket = INVALID_SOCKET;
+
+    void shutdown() {
+        if (_listen_socket != INVALID_SOCKET) {
+            closesocket(_listen_socket);
+            _listen_socket = INVALID_SOCKET;
+        }
+        if (_client_socket != INVALID_SOCKET) {
+            closesocket(_client_socket);
+            _client_socket = INVALID_SOCKET;
+        }
+        WSACleanup();
+        std::cout << "The server shutdown";
+    }
+
+public:
+    explicit ServerSocket(const int port) {
+        WSADATA wsa_data;
+        if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0) {
+            /* инициализация WSADATA версии 2.2 */
+            std::cout << "WSAStartup failed!\n";
+            throw FailedStartSocketException();
+        }
+        /* создаем сокет */
+        _listen_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+        if (_listen_socket == INVALID_SOCKET) {
+            std::cout << "Socket creation failed!\n";
+            WSACleanup();
+            throw FailedStartSocketException();
+        }
+
+        sockaddr_in server_address{};
+        server_address.sin_family = AF_INET;
+        InetPton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+        server_address.sin_port = htons(port);
+
+        if (bind(_listen_socket, reinterpret_cast<sockaddr *>(&server_address), sizeof(server_address)) ==
+            SOCKET_ERROR) {
+            std::cout << "Bind failed!\n";
+            closesocket(_listen_socket);
+            WSACleanup();
+            throw FailedStartSocketException();
+        }
+    }
+
+    void start_listen_socket() {
+        if (listen(_listen_socket, 1) == SOCKET_ERROR) {
+            std::cout << "Listen failed!\n";
+            shutdown();
+            throw FailedStartSocketException();
+        }
+        std::cout << "Start listening socket!\n";
+        _client_socket = accept(_listen_socket, nullptr, nullptr);
+        if (_client_socket == INVALID_SOCKET) {
+            std::cout << "Accept failed!\n";
+            shutdown();
+            throw FailedStartSocketException();
+        }
+        std::cout << "Server accepted!\n";
+    }
+
+    /**
+     *
+     * @param request Строка, в которую нужно положить данные, отправленные пользователем
+     * @return Количество считанных байт
+     */
+    int get_client_request(std::string &request) {
+        /* считываем длину сообщения */
+        uint32_t length_data;
+        int count_bytes = recv(_client_socket, reinterpret_cast<char *>(&length_data), sizeof(length_data), 0);
+        if (count_bytes == SOCKET_ERROR) {
+            std::cout << "Receive data from client failed!\n";
+            shutdown();
+            throw FailedReceiveDataException();
+        }
+        if (count_bytes == 0) {
+            std::cout << "Client closed connection!\n";
+            shutdown();
+            return 0;
+        }
+
+        if (length_data == 0) {
+            request = "";
+            return 1;
+        }
+
+        /* считываем само сообщение */
+        request.resize(length_data);
+        count_bytes = recv(_client_socket, request.data(), static_cast<int>(length_data), 0);
+        if (count_bytes == SOCKET_ERROR) {
+            std::cout << "Receive data from client failed!\n";
+            shutdown();
+            throw FailedReceiveDataException();
+        }
+        if (count_bytes == 0) {
+            std::cout << "Client closed connection!\n";
+            shutdown();
+        }
+        return count_bytes;
+    }
+
+    void send_response_to_client(const std::string &response) {
+        /* сначала отправляем длину ответа */
+        const uint32_t length_data = response.size();
+        if (send(_client_socket, reinterpret_cast<const char *>(&length_data), sizeof(length_data) , 0) == SOCKET_ERROR) {
+            std::cout << "Send data to client failed!\n";
+            shutdown();
+            throw FailedSendDataException();
+        }
+
+        /* затем отправляем сами данные */
+        if (send(_client_socket, response.data(), static_cast<int>(length_data), 0) == SOCKET_ERROR) {
+            std::cout << "Send data to client failed!\n";
+            shutdown();
+            throw FailedSendDataException();
+        }
+    }
+
+    ~ServerSocket() {
+        shutdown();
+    }
+};
+
+void help() {
+    std::cout << "Usage: ./main_server PORT\n";
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        help();
+        return -1;
+    }
+
+    int port;
+    try {
+        port = std::stoi(argv[1]);
+    } catch (std::exception &) {
+        std::cout << "Invalid format port!\n";
+        help();
+        return -1;
+    }
+    ServerSocket server_socket(port);
+    server_socket.start_listen_socket();
+
+    std::string request;
+    while (server_socket.get_client_request(request) != 0) {
+        nlohmann::json response;
+        if (request.empty()) {
+            /* Bad request */
+            response = {{"Message", "Bad request"}, {"Status", "400"}};
+        }
+        else {
+            // TODO: handle client request
+            response = nlohmann::json::parse(request);
+        }
+        server_socket.send_response_to_client(to_string(response));
+    }
+
+    return 0;
+}
