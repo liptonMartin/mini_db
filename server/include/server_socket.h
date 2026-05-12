fc9fc513ca3ea7f87fac1ce6e08a5ad8c5cd32ce
lipton <liptonmartin135@gmail.com>
2026-05-12 10:52:36 +0300

    add tls checks for connection: server-client

--- server/include/server_socket.h
+++ server/include/server_socket.h
@@ -4,9 +4,14 @@
 #ifndef MINIDB_SERVER_SOCKET_H
 #define MINIDB_SERVER_SOCKET_H
 
+#include <boost/asio/ssl.hpp>
+
 #include "entrypoint.h"
 
-using boost_generator = boost::uuids::random_generator;
+using BoostGenerator = boost::uuids::random_generator;
+
+using AsioSSLSocket = boost::asio::ssl::stream<AsioSocket>;
+using AsioSSLSocketPtr = std::shared_ptr<AsioSSLSocket>;
 
 /**
  * Обертка над клиентским соединением.
@@ -14,20 +19,23 @@ using boost_generator = boost::uuids::random_generator;
  * в будущем будет поддерживать распределение прав (какие команды может выполнять)
  */
 struct ClientSession {
-    asio_socket_ptr client_socket;
+    AsioSSLSocketPtr ssl_client_socket;
     std::string db_name = "";
 };
 
 class ServerSocket {
-    boost::uuids::random_generator& _generator;
-    Entrypoint& _entrypoint;
+    boost::uuids::random_generator &_generator;
+    Entrypoint &_entrypoint;
 
     boost::asio::io_context _io_context;
+    boost::asio::ssl::context _ssl_context;
     boost::asio::ip::tcp::acceptor _acceptor;
 
     std::shared_ptr<spdlog::logger> _logger;
 
     void async_start_accept();
+
+    void async_handshake(const AsioSSLSocketPtr& ssl_client_socket);
 
     void async_read_header(const ClientSession &session);
 
@@ -35,7 +43,7 @@ class ServerSocket {
 
     void async_handle_request(const ClientSession &session, const std::shared_ptr<std::string> &request);
 
-    void async_send_task_id_client(const ClientSession &session, const boost::uuids::uuid& task_id);
+    void async_send_task_id_client(const ClientSession &session, const boost::uuids::uuid &task_id);
 
     void async_send_response_client(const ClientSession &session, const nlohmann::json &response);
 
@@ -44,7 +52,7 @@ class ServerSocket {
     static nlohmann::json get_message_bad_request();
 
 public:
-    explicit ServerSocket(Entrypoint& entrypoint, boost_generator& generator, int port);
+    explicit ServerSocket(Entrypoint &entrypoint, BoostGenerator &generator, int port);
 
     ~ServerSocket();
 };
